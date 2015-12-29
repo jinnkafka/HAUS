5e396abff7500cb36a3c2d607d9413a09824b451
Vincent Jin <chenjin@usc.edu>
2015-12-29 01:57:41 -0800

    first commit

---
+++ CourseTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CourseTableViewCell.swift
+//  HAUS
+//
+//  Created by Chen Jin on 12/28/15.
+//  Copyright © 2015 Chen Jin. All rights reserved.
+//
+/*
+import UIKit
+
+class CourseTableViewCell: PFTableViewCell {
+    
+    @IBOutlet weak var courseLabel: UILabel!
+    
+}
+*/
---
+++ Podfile
@@ -0,0 +1,18 @@
+
+platform :ios, '8.0'
+
+use_frameworks!
+
+target 'HAUS' do
+	pod 'Parse'
+	pod 'ParseUI'
+end
+
+target 'HAUSTests' do
+
+end
+
+target 'HAUSUITests' do
+
+end
+
